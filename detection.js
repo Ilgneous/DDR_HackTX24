cc454f3c93de972936928e6a705309c00ace5043
Sivan Syed <Sivan Syed>
2024-11-02 20:07:56 -0500

    Commenting detection code

--- detection.js
+++ detection.js
@@ -1,8 +1,11 @@
 import cv from "@techstark/opencv-js"
 
-let video = document.getElementById('videoInput');
-let cap = new cv.VideoCapture(video);
+let video = document.getElementById('videoInput'); // Search for element 'videoInput' and assign
+let cap = new cv.VideoCapture(video); // cv.VideoCapture() is constructor by OpenCV to capture framed from video source
 
+// Store the matrix of the frames and the mask, which will hold the foreground
+// CV - 4 Channels (Red, Green, Blue, Alpha)
+// 8U - 8-bit unsigned integer per channel
 let frame = new cv.Mat(video.height, video.width, cv.CV_8UC4);
 let fgmask = new cv.Mat(video.height, video.width, cv.CV_8UC1);
 let fgbg = new cv.BackgroundSubtractorMOG2(500, 16, false);
